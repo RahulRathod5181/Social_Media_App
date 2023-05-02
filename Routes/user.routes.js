1a8902f932e57cf538d1ca35cc583108d6beb067
RahulRathod5181 <rahulrathod5181@gmail.com>
2023-05-02 15:17:34 +0530

    till login

--- Routes/user.routes.js
+++ Routes/user.routes.js
@@ -1,9 +1,9 @@
 const {UserModel} = require("../Model/User.model")
 const bcrypt = require('bcrypt');
 const express = require("express");
-
+var jwt = require('jsonwebtoken');
 const userRouter = express.Router();
-
+require("dotenv").config()
 
 userRouter.post("/register",async(req,res)=>{
     const {name,email,gender,password} = req.body;
@@ -20,7 +20,26 @@ userRouter.post("/register",async(req,res)=>{
 })
 
 userRouter.post("/login",async(req,res)=>{
-    
+    const {email,password} = req.body;
+    const user = await UserModel.findOne({email});
+    try {
+        if(user){
+            bcrypt.compare(password, user.password, (err, result)=> {
+                // result == true
+                if(result){
+                    var token = jwt.sign({userID:user._id, postBy:user.name}, process.env.Secret);
+                    res.status(200).send({msg:"Login Succesfull",token:token})
+                    
+                }else{
+                    res.status(200).send({msg:"Wrong Credientials!!"})
+                }
+            });
+        }else{
+            res.status(200).send({msg:"Wrong Credientials!!"})
+        }
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 })
 
 
